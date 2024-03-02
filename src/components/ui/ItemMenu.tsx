39ef49527b700147fa71ce50ba73ac6100a89606
Jostin Aguilar <aguilarjostin2001@gmail.com>
2024-03-02 18:24:47 -0500

    add navbar responsive and submenu and subitems

--- src/components/ui/ItemMenu.tsx
+++ src/components/ui/ItemMenu.tsx
@@ -1,3 +1,5 @@
+import { clsx } from 'clsx';
+import { useState } from 'react';
 import { CategoriesType } from '../../types/types';
 import { ChevronRightIcon } from './Icons';
 
@@ -7,9 +9,24 @@ interface Props {
 }
 
 export function ItemMenu({ name, submenu }: Props) {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openSubmenu, setOpenSubmenu] = useState(false);
+
+  const handleToggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const handleToggleSubmenu = () => {
+    setOpenSubmenu(!openSubmenu);
+  };
+
   return (
     <li className="w-full">
-      <a href="#" className="flex flex-row items-center justify-between">
+      <a
+        href="#"
+        className="flex flex-row items-center justify-between"
+        onClick={handleToggleMenu}
+      >
         <div className="flex flex-row items-center gap-3 text-sm font-bold lg:flex-col">
           <img
             src={`/src/assets/categories/${name}.svg`}
@@ -19,27 +36,42 @@ export function ItemMenu({ name, submenu }: Props) {
           {name}
         </div>
         {submenu.length > 0 && (
-          <span className="text-pumpkin-600">
+          <span
+            className={clsx(
+              'rotate-0 text-pumpkin-600 transform duration-300 transition',
+              {
+                'rotate-90': openMenu,
+              }
+            )}
+          >
             <ChevronRightIcon />
           </span>
         )}
       </a>
-      {submenu.length > 0 && (
+      {openMenu && submenu.length > 0 && (
         <ul className="mt-2">
           {submenu.map((subitem) => (
             <li key={subitem.id} className="w-full pl-9">
               <a
                 href="#"
                 className="inline-flex w-full py-1.5 text-xs items-center justify-between font-semibold text-zinc-800"
+                onClick={handleToggleSubmenu}
               >
                 {subitem.nom}
                 {subitem.sub_menus.length > 0 && (
-                  <span className="inline-flex w-5 text-zinc-500">
+                  <span
+                    className={clsx(
+                      'inline-flex w-5 mr-1 transform duration-300 transition text-zinc-500',
+                      {
+                        'rotate-90': openSubmenu,
+                      }
+                    )}
+                  >
                     <ChevronRightIcon />
                   </span>
                 )}
               </a>
-              {subitem.sub_menus.length > 0 && (
+              {openSubmenu && subitem.sub_menus.length > 0 && (
                 <ul>
                   {subitem.sub_menus.map((item: CategoriesType) => (
                     <li key={item.id}>
